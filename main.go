ec75255a225a40aaa364a47a1d991fb3ec01a689
Ramdhani11 <anwarramdhanie@gmail.com>
2024-01-23 23:51:08 +0700

    Handle Cors

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
-﻿package main
+package main
 
 import (
 	"ato_chat/chat"
 	"ato_chat/config"
 	"ato_chat/translation"
 	"ato_chat/web"
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
@@ -49,26 +48,12 @@ func main() {
 	// Create HTTP server
 	server := web.NewServer(conversationRepo, gpt4Translator)
 
-	// Where ORIGIN_ALLOWED is like scheme://dns[:port], or * (insecure)
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
-	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
-
-	// start server listen
-	// with error handling
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), handlers.CORS(originsOk, headersOk, methodsOk)(server.Router)))
-
-	// Create a context that listens for the interrupt signal from the OS
-	_, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// Get the server port from the environment or .env file
-	port := os.Getenv("PORT_SERVER")
-	if port == "" {
-		port = "8080" // Port default jika tidak ditemukan
-	}
-
 	// Start HTTP server
+	port := "8080"
 	fmt.Printf("Server is running on port %s...\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, server.Router))
+	log.Fatal(http.ListenAndServe(":"+port,handlers.CORS(
+		handlers.AllowedOrigins([]string{"http://localhost:5173"}),
+		handlers.AllowedMethods([]string{"GET","POST","DELETE","PUT","OPTIONS"}),
+		handlers.AllowedHeaders([]string{"X-Requested-With","Content-Type","Authorization"}),
+	)(server.Router)))
 }
