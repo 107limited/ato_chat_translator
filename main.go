bb4a8041f21bd57d9b38c618586cff4ca7a99627
ilhamfw <ilham.fw18@gmail.com>
2024-01-23 11:19:16 +0700

    modify main.go :

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/rs/cors"
-	//"github.com/rs/cors"
+	
+	
 )
 
 func main() {
@@ -50,14 +50,11 @@ func main() {
 	// Create HTTP server
 	server := web.NewServer(conversationRepo, gpt4Translator)
 
-	// Create CORS handler with specific configurations
-    corsHandler := cors.New(cors.Options{
-        AllowedOrigins: []string{"*"},
-        ExposedHeaders: []string{"Access-Control-Allow-Origin"}, 
-    }).Handler
+	// Create CORS middleware
+    corsMiddleware := CORSMiddleware(server.Router)
 
-    // Attach the CORS handler before your routes
-    server.Router.Use(corsHandler)
+    // Attach the CORS middleware before your routes
+    http.Handle("/", corsMiddleware)
 
 	// Create a context that listens for the interrupt signal from the OS
 	_, cancel := context.WithCancel(context.Background())
@@ -72,4 +69,22 @@ func main() {
 	// Start HTTP server
 	fmt.Printf("Server is running on port %s...\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, server.Router))
+}
+
+// CORSMiddleware menangani kebijakan CORS
+func CORSMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		
+
+        if r.Method == http.MethodOptions {
+            // Handling preflight request
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
 }
