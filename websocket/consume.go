7c76ca5929d577b67c8b8074424889161cebc2c5
ilhamfw <ilham.fw18@gmail.com>
2024-02-28 10:24:30 +0700

    modify consume.go

--- websocket/consume.go
+++ websocket/consume.go
@@ -3,50 +3,42 @@ package websocket
 import (
 	"ato_chat/models"
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
-// chat_room_id: number;
-// company_id: number;
-// created_at: string;
-// date: number;
-// english_text: string;
-// id: number;
-// japanese_text: string;
-// speaker: string;
-// user_id: number;
-
+// Response struct as provided.
 type Response struct {
 	Conversations *models.ConversationWebsocket `json:"conversations"`
-	Sidebar SidebarMessage `json:"sidebar"`
+	Sidebar       SidebarMessage                `json:"sidebar"`
 }
 
 
 func HandleWSL(w http.ResponseWriter, r *http.Request) {
-	
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		// Handle error
+		log.Printf("WebSocket upgrade failed: %v", err)
 		return
 	}
 	defer conn.Close()
+	log.Println("WebSocket connection successfully upgraded.")
 
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
-			// Handle error
+			log.Printf("Error reading message: %v", err)
 			break
 		}
 
-		// Unmarshal JSON from the incoming message
 		var message *models.ConversationWebsocket
 		err = json.Unmarshal(msg, &message)
 		if err != nil {
-			// Handle error
+			log.Printf("Error unmarshaling message: %v", err)
 			break
 		}
+		log.Println("Message successfully unmarshaled.")
 
 		
 
@@ -63,47 +55,45 @@ func HandleWSL(w http.ResponseWriter, r *http.Request) {
 	// Date,        
 	// 	}
 
-		lastmessage := LastMessage{
-			UserID:   message.UserID,
-			English:  message.EnglishText,
-			Japanese: message.JapaneseText,
-			Date:     message.Date,
-		}
-		
-		var company string
-
-		if message.CompanyID == 1 {
-			 company = "ATO"
-		}else {
-			company = "107"
-		}
-
-		sidebar := SidebarMessage{
-			UserID:      message.UserID2,
-			CompanyName: company,
-			Name:        message.UserName,
-			ChatRoomID:  message.ChatRoomID,
-			CreatedAt:   "", 
-			LastMessage: lastmessage,
-		}
-
-		responseMssg := Response{
-			Sidebar:      sidebar,
-			Conversations: message,
-		}
-
-		// Marshal the modified object back to JSON
-		responseMsg, err := json.Marshal(responseMssg)
-		if err != nil {
-			// Handle error
-			break
-		}
-
-		// Send the JSON response back to the client
-		err = conn.WriteMessage(websocket.TextMessage, responseMsg)
-		if err != nil {
-			// Handle error
-			break
-		}
+	lastMessage := LastMessage{
+		UserID:   message.UserID,
+		English:  message.EnglishText,
+		Japanese: message.JapaneseText,
+		Date:     message.Date,
 	}
+
+	var company string
+	if message.CompanyID == 1 {
+		company = "ATO"
+	} else {
+		company = "107"
+	}
+
+	sidebar := SidebarMessage{
+		UserID:      message.UserID2,
+		CompanyName: company,
+		Name:        message.UserName,
+		ChatRoomID:  message.ChatRoomID,
+		CreatedAt:   "",
+		LastMessage: lastMessage,
+	}
+
+	responseMsg := Response{
+		Sidebar:       sidebar,
+		Conversations: message,
+	}
+
+	responseJSON, err := json.Marshal(responseMsg)
+	if err != nil {
+		log.Printf("Error marshaling response: %v", err)
+		break
+	}
+
+	err = conn.WriteMessage(websocket.TextMessage, responseJSON)
+	if err != nil {
+		log.Printf("Error sending message: %v", err)
+		break
+	}
+	log.Println("Message successfully sent to the client.")
+}
 }
